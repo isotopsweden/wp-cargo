003478d22064d984fd852db006cd4172bdc2b8f4
Fredrik Forsmo <frozzare@users.noreply.github.com>
2017-03-17 11:45:34 +0100

    Fix so right user token is created

--- src/content/class-user.php
+++ src/content/class-user.php
@@ -25,7 +25,7 @@ class User extends Abstract_Content {
 		}
 
 		$this->create( 'user', [
-			'token' => cargo_user_token(),
+			'token' => cargo_user_token( $user ),
 			'user'  => [
 				'id'   => intval( $user->data->ID ),
 				'user' => $user->data->user_login
